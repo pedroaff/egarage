adc13a221d57df1f0d0ed3654375a9858ff30de2
pedroaff <pedroafaria91@gmail.com>
2019-11-08 14:11:59 -0300

    retrieving profile information

--- src/views/Perfil.vue
+++ src/views/Perfil.vue
@@ -1,8 +1,16 @@
 <template>
     <div>
         <h1>Perfil</h1>
-        <p>{{ $route.params.tipo }}</p>
-        <p>{{ $route.params.id }}</p>
+        <p>{{ this.usuario.id }}</p>
+        <p>{{ this.usuario.nome }}</p>
+        <p>{{ this.usuario.cep }}</p>
+        <p>{{ this.usuario.cpf }}</p>
+        <p>{{ this.usuario.telefone }}</p>
+        <p>{{ this.usuario.logradouro }}</p>
+        <p>{{ this.usuario.complemento }}</p>
+        <p>{{ this.usuario.cep }}</p>
+        <p>{{ this.usuario.tipo }}</p>
+        <p></p>
     </div>
 </template>
 
@@ -11,33 +19,21 @@ import axios from 'axios'
 export default {
   data () {
     return {
-      fields: [
-        {
-          key: 'id',
-          sortable: true
-        },
-        {
-          key: 'nome',
-          sortable: true
-        },
-        {
-          key: 'email',
-          sortable: true
-        },
-        {
-          key: 'tipo',
-          sortable: true
-        }
-      ],
-      usuarios: []
+      params: {},
+      usuario: {}
     }
   },
+  created () {
+      this.params = this.$route.params
+  },
   mounted () {
-    axios({ method: 'GET', 'url': 'http://localhost:8080/usuarios/' + this.$route.params.id }).then(result => {
+    axios({ method: 'GET', 'url': 'http://localhost:8080/usuarios/' + this.params.id }).then(result => {
       this.usuario = result.data
-      console.log(this.usuarios)
+      console.log(this.usuario)
+      console.log(this.params.id)
       console.log('foi')
     }, error => {
+      console.log(this.params.id)
       console.error(error)
     })
   }
