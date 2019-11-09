52453a7c7b689934165452ac7d06b5878047b9a9
pedroaff <pedroafaria91@gmail.com>
2019-11-09 19:47:20 -0300

    better layout for customer form

--- src/views/AddCliente.vue
+++ src/views/AddCliente.vue
@@ -1,98 +1,119 @@
 <template>
   <div>
+    <h1 align="left">Criar novo cliente</h1>
+    <br>
     <b-form @submit="onSubmit" @submit.stop.prevent>
-    
-      <b-form-group
-      id="fieldset-horizontal"
-      label-cols-sm="4"
-      label-cols-lg="3"
-      label="Digite o nome"
-      label-for="input-horizontal"
-    >
-        <b-form-input v-model="form.nome"></b-form-input>
+      <b-col sm="5">
+        <b-form-group
+        id="fieldset-horizontal"
+        label-cols-sm="4"
+        label-cols-lg="3"
+        label="Nome"
+        label-for="input-horizontal"
+      >
+          <b-form-input v-model="form.nome"></b-form-input>
 
-      </b-form-group>
+        </b-form-group>
+      </b-col>
 
-      <b-form-group
-      id="fieldset-horizontal"
-      label-cols-sm="4"
-      label-cols-lg="3"
-      label="Email"
-      label-for="input-horizontal"
-    >
-        <b-form-input v-model="form.email"></b-form-input>
-        
-      </b-form-group>
+      <b-col sm="5">
+        <b-form-group
+        id="fieldset-horizontal"
+        label-cols-sm="4"
+        label-cols-lg="3"
+        label="Email"
+        label-for="input-horizontal"
+      >
+          <b-form-input v-model="form.email"></b-form-input>
+          
+        </b-form-group>
+      </b-col>
 
+      <b-col sm="5">
+        <b-form-group
+        id="fieldset-horizontal"
+        label-cols-sm="4"
+        label-cols-lg="3"
+        label="Senha"
+        label-for="input-horizontal"
+      >
+          <b-form-input v-model="form.senha"></b-form-input>
+          
+        </b-form-group>
+      </b-col>
 
-      <b-form-group
-      id="fieldset-horizontal"
-      label-cols-sm="4"
-      label-cols-lg="3"
-      label="Senha"
-      label-for="input-horizontal"
-    >
-        <b-form-input v-model="form.senha"></b-form-input>
-        
-      </b-form-group>
+      <b-col sm="5">
+        <b-form-group
+        id="fieldset-horizontal"
+        label-cols-sm="4"
+        label-cols-lg="3"
+        label="CPF"
+        label-for="input-horizontal"
+      >
+          <b-form-input v-model="form.cpf"></b-form-input>
+          
+        </b-form-group>
+      </b-col>
 
+      <b-col sm="5">
+        <b-form-group
+        id="fieldset-horizontal"
+        label-cols-sm="4"
+        label-cols-lg="3"
+        label="Telefone"
+        label-for="input-horizontal"
+      >
+          <b-form-input v-model="form.telefone"></b-form-input>
+          
+        </b-form-group>
+      </b-col>
 
-      <b-form-group
-      id="fieldset-horizontal"
-      label-cols-sm="4"
-      label-cols-lg="3"
-      label="CPF"
-      label-for="input-horizontal"
-    >
-        <b-form-input v-model="form.cpf"></b-form-input>
-        
-      </b-form-group>
+      <b-col sm="5">
+        <b-form-group
+        id="fieldset-horizontal"
+        label-cols-sm="4"
+        label-cols-lg="3"
+        label="CEP"
+        label-for="input-horizontal"
+      >
+          <b-form-input v-model="form.cep"></b-form-input>
+          
+        </b-form-group>
+      </b-col>
 
-      <b-form-group
-      id="fieldset-horizontal"
-      label-cols-sm="4"
-      label-cols-lg="3"
-      label="Telefone"
-      label-for="input-horizontal"
-    >
-        <b-form-input v-model="form.telefone"></b-form-input>
-        
-      </b-form-group>
+      <b-col sm="5">
+        <b-form-group
+        id="fieldset-horizontal"
+        label-cols-sm="4"
+        label-cols-lg="3"
+        label="Logradouro"
+        label-for="input-horizontal"
+      >
+          <b-form-input v-model="form.logradouro"></b-form-input>
+          
+        </b-form-group>
+      </b-col>
 
-      <b-form-group
-      id="fieldset-horizontal"
-      label-cols-sm="4"
-      label-cols-lg="3"
-      label="CEP"
-      label-for="input-horizontal"
-    >
-        <b-form-input v-model="form.cep"></b-form-input>
-        
-      </b-form-group>
+      <b-col sm="5">
+        <b-form-group
+        id="fieldset-horizontal"
+        label-cols-sm="4"
+        label-cols-lg="3"
+        label="Complemento"
+        label-for="input-horizontal"
+      >
+          <b-form-input v-model="form.complemento"></b-form-input>
+          
+        </b-form-group>
+      </b-col>
 
-      <b-form-group
-      id="fieldset-horizontal"
-      label-cols-sm="4"
-      label-cols-lg="3"
-      label="Logradouro"
-      label-for="input-horizontal"
-    >
-        <b-form-input v-model="form.logradouro"></b-form-input>
-        
-      </b-form-group>
+      <b-button class="float-left m-3" type="submit" variant="primary">Salvar</b-button>
 
-      <b-form-group
-      id="fieldset-horizontal"
-      label-cols-sm="4"
-      label-cols-lg="3"
-      label="Complemento"
-      label-for="input-horizontal"
-    >
-        <b-form-input v-model="form.complemento"></b-form-input>
-        
-      </b-form-group>
-
-      <b-button class="float-left" type="submit" variant="primary">Salvar</b-button>
+      <router-link :to="`/usuarios/`">
+          <b-button class="float-left mr-2 m-3" variant="danger">
+                Voltar
+          </b-button>
+      </router-link>
 
      </b-form>
   </div>
@@ -145,3 +166,11 @@ import axios from 'axios'
     }
   }
 </script>
+
+<style>
+.col-form-label {
+  text-align: "left"
+}
+
+
+</style>
